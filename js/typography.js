a70cc8e57b1f219939036e17ea81bc73ce29411c
elliottcable <git@elliottcable.name>
2009-02-26 00:10:09 -0900

    Getting started on a method to "underline" some text.

--- js/typography.js
+++ js/typography.js
@@ -62,4 +62,74 @@ var create_calculation_area = function(content) {
   return calculation_area;
 };
 
+// ////////
+// This function draws an underline directly under an element's text.
+Typography.underline = function(element) {
+  // We require an `element` argument to be present
+  if(typeof element != "object" || element == null) return false;
+  // We require that `element` duck type to an `Element`
+  if(typeof element.getStyle != "function") return false;
+  
+  var baseline = Typography.baseline(element)
+  if(!baseline) return false;
+  
+  // Our approach is essentially to create a new copy of `element, and then
+  // wrap all of the copy's content in a `<span>`. This `display: inline;`
+  // duplicate will be given a `border-bottom`, and then the original element
+  // will be padded in such a way as to align the border of the duplicate to
+  // the baseline of the original.
+  // 
+  // This is necessary for several reasons; primarily, we don't want the
+  // border to appear anywhere there isn't text, because that's how an
+  // underline is expected to function.
+  var duplicate = element.cloneNode(true);
+  
+  // Absolute positioning only works within a positioned containing element.
+  if(element.getStyle('position') == 'static')
+    element.setStyle('position', 'relative');
+  
+  // We're going to place our duplicate directly underneath the existing text …
+  duplicate.inject(element);
+  duplicate.setStyle('position', 'absolute');
+  duplicate.setStyle('top', '0px');
+  duplicate.setStyle('left', '0px');
+  duplicate.setStyle('z-index', zIndex(element) - 1);
+  
+  duplicate.setStyle('color', 'red'); // DEBUG
+  
+  // … then create a new span, and move all of the duplicate's contents into it …
+  var wrapper = new Element('span');
+  // (Unfortunately, getChildren only returns elements, not all nodes.)
+  //duplicate.getChildren().each(function(el) { el.inject(wrapper); });
+  var curr, next = duplicate.firstChild;
+  while (curr = next) {
+    next = next.nextSibling
+    wrapper.adopt(curr);
+  };
+  wrapper.inject(duplicate);
+  
+  // … and then add a bottom-border to that span!
+  wrapper.setStyle('border-bottom', '1px solid red');
+  
+  return true;
+}
+
+// Returns the computed `z-index` of a given `element`, iterating up the DOM
+// until it reaches an element with a defined `z-index`.
+var zIndex = function(element) {
+  if(typeof console != "undefined") console.log(element); // DEBUG
+  // We require an `element` argument to be present
+  if(typeof element != "object" || element == null) return false;
+  // We require that `element` duck type to an `Element`
+  if(typeof element.getStyle    != "function" ||
+     typeof element.parentNode  != "object")
+    return false;
+  
+  var css_z_index = element.getStyle('z-index');
+  // Something went wrong, and we reached the root document without even a '0'
+  if(css_z_index == null) return false;
+  var z_index = ((css_z_index == "auto") ? zIndex(element.parentNode) : parseInt(css_z_index))
+  return z_index;
+}
+
 })();
